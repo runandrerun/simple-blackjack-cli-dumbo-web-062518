aa68330ff3413204146d3b38d57d7906a092a5a9
Andre Santiago <the.asantiagojr@gmail.com>
2018-06-18 17:14:34 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -8,6 +8,7 @@ end
 
 def display_card_total(card_total)
   # code #display_card_total here
+  
   puts "Your cards add up to #{card_total}"
 end
 
@@ -28,6 +29,9 @@ end
 
 def initial_round
   # code #initial_round here
+  deal_card
+  deal_card
+  display_card_total
 end
 
 def hit?
